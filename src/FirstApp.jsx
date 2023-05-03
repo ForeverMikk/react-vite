96b20f14ed9f1580dbfa8e26b58d02326de44b07
unknown <michaelmolina679@gmail.com>
2023-05-03 13:14:56 -0600

    estilos en css

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,21 +1,11 @@
 
-
-const newMessage = {
-    message: "Hola Mundo",
-    title: "Michael"
-}
-
-const getResult = () => {
-    return 4 + 4;
-}
-
 const App = () => {
 
     return (
         // Componente Frafment <></> se usa para encapsular tu componente en React
         // sin necesitar un <div> o alguna otra etiqueta
         <>
-            <h1>{getResult()}</h1>
+            <h1>Michael</h1>
             <p>Soy un subtutilo</p>
         </>
     )
