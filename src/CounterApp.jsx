ad016d1dfd17aafe0cbdbff941acf78aadf33eed
unknown <michaelmolina679@gmail.com>
2023-05-03 16:31:16 -0600

    jugando con los estados en react

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,25 +1,23 @@
-import PropTypes, { string } from 'prop-types';
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
     const [counter, setCounter] = useState(value);
 
-    const handleAdd = () => {
-        setCounter(counter + 1);
-    }
-    const handleSubstract = () => {
-        setCounter(counter - 1);
-    }
-    const handleReset = () => {
-        setCounter(value);
+    const handleAdd = () => setCounter(counter + 1);
+    const handleSubstract = () => setCounter(counter - 1);
+    const handleReset = () => setCounter(value);
+
+    const handleChange = (isChanging) => {
+        return isChanging ? setCounter(counter + 1) : setCounter(counter - 1);
     }
 
     return (
         <>
             <h1>Counter App</h1>
             <p>valor: { counter }</p>
-            <button onClick={ handleAdd }>+1</button>
-            <button onClick={ handleSubstract }>-1</button>
+            <button onClick={ () => handleChange(true) }>+1</button>
+            <button onClick={ () => handleChange(false) }>-1</button>
             <button onClick={ handleReset }>Reset</button>
         </>
     )
