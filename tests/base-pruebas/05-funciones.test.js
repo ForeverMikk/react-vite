421816535f7e88d97d0bcdad675528915e60f80d
unknown <michaelmolina679@gmail.com>
2023-05-12 13:05:55 -0600

    pruebas a 05-funciones, pruebas con objetos

--- tests/base-pruebas/05-funciones.test.js
+++ tests/base-pruebas/05-funciones.test.js
@@ -1,1 +1,30 @@
-describe('Pruebas en 05-funciones', () => { second })
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones"
+
+describe('Pruebas en 05-funciones', () => { 
+
+    test('getUser debe retornar un objeto', () => { 
+
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        }
+
+        const user = getUser();
+        // ara evaluar un objeto se usa el toStrictEqual() 
+        // porque un objeto no apunta al mismo lugar de memoria
+        expect(testUser).toEqual(user);
+     });
+
+
+     test('getUsuarioActivo debe retornar un objeto ', () => { 
+
+        const name = 'Fernando';
+
+        const testUserActivo = {
+            uid: 'ABC567',
+            username: name
+        }
+
+        expect(testUserActivo).toEqual(getUsuarioActivo(name));
+      })
+ })
